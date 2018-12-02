1373eec614eeb22f463fb775696eec0a89805afb
shady <shady@xps13.lan>
2018-12-02 23:22:49 +0000

    Update

--- init.el
+++ init.el
@@ -12,9 +12,6 @@
 	(package-refresh-contents)
 	(package-install 'use-package))
 
-;; install packages if necessary
-(setq use-package-always-ensure t)
-
 ;; auto update packages
 (use-package auto-package-update
   :config
@@ -32,7 +29,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages (quote (auto-package-update use-package))))
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
