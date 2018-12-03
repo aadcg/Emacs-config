7038009a68585a296104b372795217906afdb00e
Andre Alexandre da Costa Gomes <aadco@vestas.com>
2018-12-03 08:53:36 +0000

    Ensured auto-package-update

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
 
 ;; auto update packages
 (use-package auto-package-update
+  :ensure t
   :config
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t)
