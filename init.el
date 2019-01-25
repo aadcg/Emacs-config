29ca1248e6b51dcca7016758afc07e445a9ad87b
Andre Alexandre da Costa Gomes <aadco@vestas.com>
2019-01-25 12:00:31 +0000

    Clean

--- init.el
+++ init.el
@@ -22,3 +22,17 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (avy swiper org-pdfview org-bullets py-autopep8 ein elpy magit smartparens pdf-tools nord-theme diminish auto-package-update use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
