fba11b7b7921bb2c5a74057f4ca4c67a492ed692
shady <andremegafone@gmail.com>
2019-01-26 11:12:30 +0000

    wtf

--- init.el
+++ init.el
@@ -27,6 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/.emacs.d/config.org")))
  '(package-selected-packages
    (quote
     (avy swiper org-pdfview org-bullets py-autopep8 ein elpy magit smartparens pdf-tools nord-theme diminish auto-package-update use-package))))
