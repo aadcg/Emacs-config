0d6b89ec025636a891904e41162f6c8ced5ec98a
shady <andremegafone@gmail.com>
2019-01-27 23:02:51 +0000

    fixed untracked files

--- init.el
+++ init.el
@@ -21,3 +21,17 @@
   (auto-package-update-maybe))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org use-package swiper smartparens py-autopep8 org-pdfview org-bullets nord-theme magit ivy-youtube elpy ein diminish avy auto-package-update))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
