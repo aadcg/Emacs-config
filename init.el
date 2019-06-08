a6079cf9f7779d0a71996dde76735686951424b2
André Alexandre Gomes <andremegafone@gmail.com>
2019-06-08 19:50:29 +0100

    Testing CI

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Bootstrap use-package
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(use-package auto-package-update
+  :config
+  (auto-package-update-maybe))
+
+(org-babel-load-file "~/.emacs.d/config.org")
