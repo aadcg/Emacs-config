6ead056100fb07e56cf51486f4630317e59799ec
André Alexandre Gomes <andremegafone@gmail.com>
2019-06-09 11:10:08 +0100

    CI test

--- test_startup.sh
+++ test_startup.sh
@@ -2,6 +2,6 @@
 
 echo "Attempting startup..."
 
-emacs -nw --batch --eval '(load-file "~/.emacs.d/init.el")'
+emacs26 -nw --batch --eval '(load-file "~/.emacs.d/init.el")'
 
 echo "Startup successful!"
