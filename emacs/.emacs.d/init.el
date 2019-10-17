e52a0659ddf4488256cd9b9735c17a637e36de37
André Alexandre Gomes <andremegafone@gmail.com>
2019-10-17 14:38:52 +0300

    Update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,17 +20,3 @@
 (setq use-package-compute-statistics t)
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (counsel-docker zenburn-theme which-key use-package typit smex smartparens rainbow-delimiters projectile pretty-mode org-tree-slide org-pdfview org-bullets ob-ipython ob-go moody minions magit htmlize haskell-mode google-translate gitlab-ci-mode flycheck exwm expand-region elpy ein dockerfile-mode docker diff-hl default-text-scale csv-mode counsel company-jedi company-go command-log-mode cdlatex beacon avy auto-package-update auctex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-ellipsis ((t (:foreground "gray70" :underline nil)))))
