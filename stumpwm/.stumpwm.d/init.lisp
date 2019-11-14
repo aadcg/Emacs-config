b8a453f9cad6062f9a5adb14e68380a86bcd7ea3
André Alexandre Gomes <andremegafone@gmail.com>
2019-11-14 16:38:43 +0300

    update

--- stumpwm/.stumpwm.d/init.lisp
+++ stumpwm/.stumpwm.d/init.lisp
@@ -3,22 +3,21 @@
 (setq *startup-message* nil)
 (setq *startup-mode-line* t)
 
+;; Setting up and load extra modules
 (set-module-dir "~/.stumpwm.d/stumpwm-contrib")
-
-;; Modeline Battery
 (load-module "battery-portable")
 (load-module "cpu")
 (load-module "mem")
 
 (setf *mode-line-position* :top
-      *mode-line-timeout* 20
+      *mode-line-timeout* 10
+      *time-modeline-string* "%a %d %b %H:%M"
       ;; *mode-line-pad-x* 8
       ;; *mode-line-pad-y* 1
-      *time-modeline-string* "%a %d %b %H:%M"
       ;; *window-border-style* :thin
-      *window-border-style* 1
-      *frame-number-map* "1234567890"
-      *window-format* "%m%n%s%c"
+      ;; *window-border-style* 1
+      ;; *frame-number-map* "1234567890"
+      ;; *window-format* "%m%n%s%c"
       *cpu-modeline-fmt* "%C"
       *delim* " ^2::^7 "
       *screen-mode-line-format* (list
@@ -30,7 +29,9 @@
                                  ;; RAM
                                  "%M" *delim*
                                  ;; Battery
-                                 "%B" *delim*
+                                 ;; '(:eval (stumpwm:run-shell-command "cat /sys/class/power_supply/BAT0/capacity | tr -d '\n'" t))
+                                 "%B"
+                                 *delim*
                                  ;; Date
                                  "%d"
                                  )
@@ -43,19 +44,19 @@
 (define-key *root-map* (kbd "B") "firefox")
 
 ;; launch terminal
-(defcommand urxvt () ()
-  "Start Urxvt or switch to it, if it is already running."
-  (run-or-raise "urxvt" '(:class "Urxvt")))
-(define-key *root-map* (kbd "c") "urxvt")
+;; (defcommand urxvt () ()
+;;   "Start Urxvt or switch to it, if it is already running."
+;;   (run-or-raise "urxvt" '(:class "Urxvt")))
+;; (define-key *root-map* (kbd "c") "urxvt")
 
 ;; Change xterm to urxvt
-(define-key *root-map* (kbd "C") "exec urxvt")
+;; (define-key *root-map* (kbd "C") "exec urxvt")
 
 ;; Volume Management
-(progn
-  (define-key *top-map* (kbd "XF86AudioLowerVolume") "exec amixer -q sset Master 3%-")
-  (define-key *top-map* (kbd "XF86AudioRaiseVolume") "exec amixer -q sset Master 3%+")
-  (define-key *top-map* (kbd "XF86AudioMute") "amixer -q sset Master toggle"))
+;; (progn
+;;   (define-key *top-map* (kbd "XF86AudioLowerVolume") "exec amixer -q sset Master 3%-")
+;;   (define-key *top-map* (kbd "XF86AudioRaiseVolume") "exec amixer -q sset Master 3%+")
+;;   (define-key *top-map* (kbd "XF86AudioMute") "amixer -q sset Master toggle"))
 
 ;; Mode-line
 (progn
