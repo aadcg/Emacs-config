48abcc385a11e32bb4587204f7b99eeb117e873b
André Alexandre Gomes <andremegafone@gmail.com>
2020-01-15 18:12:09 +0300

    auto-update on

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -13,10 +13,24 @@
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
-;; (use-package auto-package-update
-;;   :config
-;;   (auto-package-update-maybe))
+(use-package auto-package-update
+  :config
+  (auto-package-update-maybe))
 
-(setq use-package-compute-statistics t)
+;; (setq use-package-compute-statistics t)
 
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme which-key use-package typit smex smartparens s3ed rainbow-delimiters projectile pretty-mode org-tree-slide org-pdfview org-bullets ob-ipython ob-go moody minions magit-popup magit htmlize haskell-mode groovy-mode google-translate gitlab-ci-mode gitignore-mode flycheck find-file-in-project expand-region ensime emms elpy ein dockerfile-mode docker diff-hl default-text-scale csv-mode counsel company-jedi company-go command-log-mode cdlatex beacon avy auto-package-update auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-ellipsis ((t (:foreground "gray70" :underline nil)))))
