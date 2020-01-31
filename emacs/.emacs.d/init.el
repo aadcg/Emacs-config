fcf387e931415c42d2dcbd8eed1fb0c34b01ca26
André Alexandre Gomes <andremegafone@gmail.com>
2020-01-31 23:11:38 +0300

    added todo

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,9 +7,12 @@
   (package-install 'use-package))
 
 (use-package auto-package-update
+  :ensure t
   :config
   (auto-package-update-maybe)
   (setq auto-package-update-prompt-before-update t
         auto-package-update-interval 14))
+
+;; TODO always ensure if non-guix system
 
 (org-babel-load-file "~/.emacs.d/config.org")
