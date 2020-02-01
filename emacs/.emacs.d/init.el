80d03656b83058a54c4b88addbbe2a454d297d80
André Alexandre Gomes <andremegafone@gmail.com>
2020-02-01 17:50:03 +0300

    ensure all packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -23,17 +23,3 @@
         auto-package-update-interval 14))
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (emms google-translate default-text-scale typit smex counsel expand-region smartparens flycheck org-bullets org-tree-slide org-pdfview cdlatex htmlize ob-go ob-ipython projectile haskell-mode ein company-jedi elpy beacon hide-mode-line avy rainbow-delimiters zenburn-theme use-package moody minions magit guix gitignore-mode diff-hl auto-package-update))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-ellipsis ((t (:foreground "gray70" :underline nil)))))
