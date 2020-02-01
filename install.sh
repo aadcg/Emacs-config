a3bf222c9d3d3bb5398290adbe14102c370c74a7
André Alexandre Gomes <andremegafone@gmail.com>
2020-02-01 21:40:04 +0300

    minor fix

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# TODO before stow make sure that those files get deleted
 stow emacs
 stow bash
 stow git
@@ -5,5 +6,4 @@ stow X
 stow cfgs
 # stow termux
 stow stumpwm
-cd ~/dotfiles/stumpwm/
-git clone https://github.com/stumpwm/stumpwm-contrib.git
+cd ~/dotfiles/stumpwm/ && git clone https://github.com/stumpwm/stumpwm-contrib.git
