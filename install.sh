7681c2098378d7bd8b0326ce206ede9fac8e1175
André Alexandre Gomes <andremegafone@gmail.com>
2020-02-02 02:50:08 +0300

    minor fix

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ stow X
 stow cfgs
 # stow termux
 stow stumpwm
-cd ~/dotfiles/stumpwm/ && git clone https://github.com/stumpwm/stumpwm-contrib.git
+cd ~/.stumpwm.d/ && git clone https://github.com/stumpwm/stumpwm-contrib.git
