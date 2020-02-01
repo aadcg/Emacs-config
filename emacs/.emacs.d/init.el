da862842c2410962275c6d69f449f87d50c4b7a5
André Alexandre Gomes <andremegafone@gmail.com>
2020-02-01 13:55:39 +0300

    ensure packages on termux

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,13 +6,16 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(when in-termux-p
+  (use-package use-package-ensure
+  :config
+  (setq use-package-always-ensure t)))
+
 (use-package auto-package-update
   :ensure t
   :config
   (auto-package-update-maybe)
   (setq auto-package-update-prompt-before-update t
         auto-package-update-interval 14))
-
-;; TODO always ensure if non-guix system
 
 (org-babel-load-file "~/.emacs.d/config.org")
