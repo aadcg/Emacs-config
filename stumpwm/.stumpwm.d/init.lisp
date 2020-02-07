2136d480d5d68def91973d8ee43903d93355fe0c
André Alexandre Gomes <andremegafone@gmail.com>
2020-02-07 15:15:56 +0300

    cleanup

--- stumpwm/.stumpwm.d/init.lisp
+++ stumpwm/.stumpwm.d/init.lisp
@@ -10,23 +10,28 @@
            '("battery-portable"
              "cpu"
              "mem"
-             "amixer"))
-;; (ql:quickload :clx-truetype)
-;; (set-font (make-instance 'xft:font :font "-*-terminus-medium-r-*-*-*-125-75-75-*-*-iso8859-15")
-;; set font
-;; (ql:quickload "clx-truetype")
-;; (load-module "ttf-fonts")
-;; (set-font (make-instance 'xft:font :family "DejaVu Sans Mono" :subfamily "Book" :size 11))
-;; (set-font "Fira Code")
+             ;; "amixer"
+             ))
 
+(setf *colors* (list "#1C2028"      ; 0 black
+                     "#CC9393"      ; 1 red
+                     "#7F9F7F"      ; 2 green
+                     "#F0DFAF"      ; 3 yellow
+                     "#8CD0D3"      ; 4 blue
+                     "#DC8CC3"      ; 5 magenta
+                     "#93E0E3"      ; 6 cyan
+                     "#ECEFF4"))    ; 7 white
 
-(setf *mode-line-timeout* 5
+(setf *mode-line-timeout* 1
       *time-modeline-string* "%a %d %b %k:%M"
-      *window-border-style* :thin
       *mode-line-background-color* "#3F3F3F"
       *mode-line-foreground-color* "#DCDCCC"
+      *normal-border-width* 0
+      *mode-line-border-width* 0
+      *window-border-style* :thin
+      *mouse-focus-policy* :click
       *window-name-source* :class
-      *delim* " ^5|^7 "
+      *delim* " ^6|^7 "
       *screen-mode-line-format* (list
                                  ;; Windows
                                  "%W ^>"
@@ -35,16 +40,12 @@
                                  ;; RAM
                                  "%M" *delim*
                                  ;; Battery
-                                 ;; '(:eval (stumpwm:run-shell-command "cat /sys/class/power_supply/BAT0/capacity | tr -d '\n'" t)) *delim*
-                                 ;; Weather
-                                 ;; '(:eval (stumpwm:run-shell-command "cat /sys/class/power_supply/BAT0/capacity | tr -d '\n'" t)) *delim*
                                  "%B" *delim*
-                                 ;; Date
+                                 ;; Date (msk and pt)
                                  "%d" *delim*
                                  '(:eval (stumpwm:run-shell-command
                                           "TZ='Europe/Lisbon' date +%k:%M" t))
                                  ))
-
 
 ;; Keybindings
 (set-prefix-key (kbd "C-i"))
@@ -53,74 +54,29 @@
 (defcommand gui-browser () ()
   "Start Icecat (Firefox otherwise) or switch to it, if it is already running."
   (run-or-raise "icecat || firefox" '(:instance "Navigator")))
-
 (define-key *root-map* (kbd "w") "gui-browser")
 
 ;; Terminal
 (defcommand xterm () ()
   "Start Xterm or switch to it, if it is already running."
   (run-or-raise "xterm" '(:instance "xterm")))
-
 (define-key *root-map* (kbd "c") "xterm")
 
 ;; Telegram
-(defcommand telegram () ()
-  "Start Telegram or switch to it, if it is already running."
-  (run-or-raise "telegram-desktop" '(:instance "telegram-desktop")))
+;; (defcommand telegram () ()
+;;   "Start Telegram or switch to it, if it is already running."
+;;   (run-or-raise "telegram-desktop" '(:instance "telegram-desktop")))
 
-(define-key *root-map* (kbd "t") "telegram")
+;; (define-key *root-map* (kbd "t") "telegram")
 
 ;; Volume Management
 (progn
   (define-key *top-map* (kbd "XF86AudioLowerVolume") "exec amixer -q sset Master 3%-")
   (define-key *top-map* (kbd "XF86AudioRaiseVolume") "exec amixer -q sset Master 3%+")
-  (define-key *top-map* (kbd "XF86AudioMute") "amixer -q sset Master toggle"))
+  (define-key *top-map* (kbd "XF86AudioMute") "exec amixer -q sset Master toggle"))
 
 (progn
   (enable-mode-line (current-screen) (current-head) t))
 
-
-;; (setf swm-gaps:*inner-gaps-size* 12
-;;       swm-gaps:*outer-gaps-size* 12
-;;       swm-gaps:*head-gaps-size* 12
-;;       swm-gaps:*gaps-on* t)
-
-;; (setf *message-window-gravity* :center
-;;       *input-window-gravity* :center
-;;       *window-border-style* :thin
-;;       *message-window-padding* 10
-;;       *maxsize-border-width* 5
-;;       *normal-border-width* 5
-;;       *transient-border-width* 2
-;;       stumpwm::*float-window-border* 2
-;;       stumpwm::*float-window-title-height* 5
-;;       *mouse-focus-policy* :click)
-
-;; (set-normal-gravity :center)
-;; (set-maxsize-gravity :center)
-;; (set-transient-gravity :center)
-
-;; (set-fg-color "#eeeeee")
-;; (set-bg-color "#1C2028")
-;; (set-border-color "#232731")
-;; (set-focus-color "#3B4252")
-;; (set-unfocus-color "#232731")
-;; (set-win-bg-color "#22272F")
-;; (set-float-focus-color "#3B4252")
-;; (set-float-unfocus-color "#232731")
-
-;; (setf *colors* (list "#1C2028"      ; 0 black
-;;                      "#BF616A"      ; 1 red
-;;                      "#A3BE8C"      ; 2 green
-;;                      "#EBCB8B"      ; 3 yellow
-;;                      "#5E81AC"      ; 4 blue
-;;                      "#B48EAD"      ; 5 magenta
-;;                      "#8FBCBB"      ; 6 cyan
-;;                      "#ECEFF4"))    ; 7 white
-
-;; ;(clx-truetype:cache-fonts)
-
-;; (set-font (make-instance 'xft:font
-;; 			 :family "Source Code Pro"
-;; 			 :subfamily "Regular"
-;; 			 :size 11))
+;; Show / Hide mode-line
+(define-key *root-map* (kbd "M") "mode-line")
