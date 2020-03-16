3e9e37f9cc0885d5e4803cd0a66dbf8696f5f429
André Alexandre Gomes <andremegafone@gmail.com>
2020-03-16 12:11:58 +0000

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 # TODO before stow make sure that those files get deleted
 stow emacs
-stow bash
-stow git
-stow X
-stow cfgs
+stow guix
 # stow termux
-stow stumpwm
-cd ~/.stumpwm.d/ && git clone https://github.com/stumpwm/stumpwm-contrib.git
+
+
+pkg upgrade
+pkg install root-repo x11-repo git gtypist huspell huspell-ru openssh stow texinfo mpv-x
+emacs-x curl man python
+mkdir NextCloud
+ln -s ~/storage/shared/Android/media/com.nextcloud.client/nextcloud/up201106482@fc.up.pt@cloud.owncube.com/org .
