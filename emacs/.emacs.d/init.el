98a8a0e6c08d2b0054eaa9daf03e47f7217891f6
André Alexandre Gomes <andremegafone@gmail.com>
2020-03-31 17:50:50 +0100

    Update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,14 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 
+;; Initialise the packages, avoiding a re-initialisation.
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
+
+;; Make sure `use-package' is available.
+;; Useless if I use guix
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -11,17 +18,16 @@
        (not (equal user-login-name "aadcg")))
   "t if using GNU Emacs on Termux")
 
-(setq vc-follow-symlinks t)
-
-;; (use-package use-package-ensure
+;; (use-package auto-package-update
+;;   :ensure t
 ;;   :config
-;;   (setq use-package-always-ensure t))
+;;   (auto-package-update-maybe)
+;;   (setq auto-package-update-prompt-before-update t
+;;         auto-package-update-interval 14))
 
-(use-package auto-package-update
-  :ensure t
-  :config
-  (auto-package-update-maybe)
-  (setq auto-package-update-prompt-before-update t
-        auto-package-update-interval 14))
+(eval-when-compile
+  (require 'use-package))
 
+(require 'org)
+(setq vc-follow-symlinks t)
 (org-babel-load-file "~/dotfiles/emacs/.emacs.d/config.org")
