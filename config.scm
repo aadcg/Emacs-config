6944b50042de2e7d092453b9d1d36b3ff0ca05c5
André Alexandre Gomes <andremegafone@gmail.com>
2020-04-06 23:06:47 +0100

    Kept only the basic packages, others moved to the user profile

--- config.scm
+++ config.scm
@@ -1,58 +1,6 @@
-(use-modules (gnu))
+(use-modules (gnu) (gnu system nss))
 (use-service-modules desktop networking ssh xorg)
-
-(define %emacs
-  (map specification->package
-       '("emacs"
-         ;; "emacs-zenburn-theme"
-         ;; "emacs-moody"
-         "emacs-minions"
-         "emacs-hide-mode-line"
-         "emacs-avy"
-         ;; "emacs-elpy"
-         "emacs-company-jedi"
-         ;; "emacs-ein"
-         "emacs-haskell-mode"
-         "emacs-go-mode"
-         ;; "emacs-company-go"
-         "emacs-auctex"
-         "emacs-magit"
-         "emacs-diff-hl"
-         ;; "emacs-gitignore-mode"
-         "emacs-yaml-mode"
-         "emacs-csv-mode"
-         "emacs-gitlab-ci-mode"
-         "emacs-dockerfile-mode"
-         "emacs-projectile"
-         "emacs-rainbow-delimiters"
-         "emacs-org"
-         "emacs-ob-ipython"
-         ;; "emacs-ob-go"
-         "emacs-htmlize"
-         ;; "emacs-org-drill"
-         ;; "emacs-org-drill-table"
-         "emacs-cdlatex"
-         ;; "emacs-org-pdfview"
-         "emacs-org-tree-slide"
-         "emacs-org-bullets"
-         "emacs-guix"
-         ;; "emacs-pretty-sha-path"
-         "emacs-flycheck"
-         ;; "emacs-flyspell"
-         "emacs-pdf-tools"
-         "emacs-smartparens"
-         "emacs-expand-region"
-         ;; "emacs-counsel"
-         "emacs-smex"
-         "emacs-company"
-         ;; "emacs-speed-type"
-         ;; "emacs-default-text-scale"
-         "emacs-google-translate"
-         "emacs-emms"
-         "emacs-telega"
-         "emacs-emojify"
-         "emacs-wttrin"
-         )))
+(use-package-modules bootloaders certs suckless wm)
 
 (operating-system
   (locale "en_US.utf8")
@@ -88,61 +36,25 @@
                     '("wheel" "netdev" "audio" "video")))
                 %base-user-accounts))
   (packages
-   (append
-    %emacs
-    (map specification->package
-         '(
-           "alsa-utils"
-           "aspell"
-           "aspell-dict-en"
-           "aspell-dict-pt-pt"
-           "aspell-dict-ru"
-           "audacity"
-           "curl"
-           "font-fira-code"
-           "font-hack"
-           "git"
-           "gnupg"
-           "gtypist"
-           "icecat"
-           "imagemagick"
-           "light"
-           "mpv"
-           "nitrogen"
-           "nss-certs"
-           "openfwwf-firmware"
-           "owncloud-client"
-           "pavucontrol"
-           "python"
-           "python-autopep8"
-           "python-black"
-           "python-flake8"
-           "python-ipython"
-           "python-jedi"
-           "python-pip"
-           "python-rope"
-           "python-virtualenv"
-           "python-yapf"
-           "grim"
-           "setxkbmap"
-           "sicp"
-           "slock"
-           "stow"
-           "sway"
-           "swaylock"
-           "texlive"
-           "tor"
-           "transmission"
-           "xmodmap"
-           "xrandr"
-           ))
-      %base-packages))
+   (append (map specification->package
+                '("light"
+                  "nss-certs"
+                  "sway"
+                  "swaylock"
+                  "i3status"
+                  "grim"
+                  "tlp"
+                  "openfwwf-firmware"
+                  "b43-tools"
+                  "tor"))
+           %base-packages))
   (services
-    (append
-     (list (service gnome-desktop-service-type)
-           (service openssh-service-type)
-           (service tor-service-type)
-           (set-xorg-configuration
-            (xorg-configuration
-             (keyboard-layout keyboard-layout))))
-     %desktop-services)))
+   (append
+    (list
+     ;; (service gnome-desktop-service-type)
+     (service openssh-service-type)
+     (service tor-service-type)
+     (set-xorg-configuration
+      (xorg-configuration
+       (keyboard-layout keyboard-layout))))
+    %desktop-services)))
