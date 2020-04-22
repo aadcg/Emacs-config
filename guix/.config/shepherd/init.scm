5491bd70a45d998df9227eacb626f77bef10800a
André Alexandre Gomes <andremegafone@gmail.com>
2020-04-22 13:00:29 +0100

    moved emacs as daemon to sway cfg

--- guix/.config/shepherd/init.scm
+++
@@ -1,5 +0,0 @@
-(load "services.scm")
-
-(register-services emacs)
-(action 'shepherd 'daemonize) ; send shepherd into background
-(for-each start (list emacs))
--- guix/.config/shepherd/services.scm
+++
@@ -1,7 +0,0 @@
-(define emacs
-  (make <service>
-    #:provides '(emacs)
-    #:requires '()
-    #:start (make-system-constructor "emacs --daemon")
-    #:stop (make-system-destructor
-            "emacsclient --eval \"(kill-emacs)\"")))
