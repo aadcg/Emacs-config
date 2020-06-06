a650822944a5812713dee13f25cc9fa63b615b05
André Alexandre Gomes <andremegafone@gmail.com>
2020-06-06 10:54:00 +0100

    adeed custom file path

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -31,7 +31,7 @@
 ;;   (setq auto-package-update-prompt-before-update t
 ;;         auto-package-update-interval 14))
 
-(setq custom-file (make-temp-file "emacs-custom-"))
+(setq custom-file (concat user-emacs-directory ".emacs-custom.el"))
 
 (setq vc-follow-symlinks t)
 (org-babel-load-file "~/dotfiles/emacs/.emacs.d/config.org")
