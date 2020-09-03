e1ff97aeb8b96fbac137b3f7850b98b46d61d1f0
André Alexandre Gomes <andremegafone@gmail.com>
2020-09-03 20:36:05 +0300

    Fixed symlink creation

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ pkg upgrade
 pkg install root-repo x11-repo git gtypist huspell huspell-ru openssh stow texinfo mpv-x
 emacs-x curl man python
 mkdir NextCloud
-ln -s ~/storage/shared/Android/media/com.nextcloud.client/nextcloud/up201106482@fc.up.pt@cloud.owncube.com/org .
+ln -s -t NextCloud/ /data/data/com.termux/files/home/storage/shared/Android/media/com.nextcloud.client/nextcloud/up201106482@fc.up.pt@cloud.owncube.com/*
