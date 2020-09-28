e68d01071e7e724f8bbc47772b1a1de6223d5cff
André Alexandre Gomes <andremegafone@gmail.com>
2020-09-28 16:05:55 +0300

    Minor update

--- guix/.config/nyxt/init.lisp
+++ guix/.config/nyxt/init.lisp
@@ -1,6 +1,8 @@
 (defvar *my-keymap* (make-keymap "my-map"))
 (define-key *my-keymap*
   "C-j" 'nyxt/web-mode:follow-hint
+  "C-J" 'nyxt/web-mode:follow-hint-new-buffer
+  "C-u C-J" 'nyxt/web-mode:follow-hint-new-buffer-focus
   "C-x C-b" 'nyxt:list-buffers)
 
 (define-mode my-mode ()
@@ -8,33 +10,51 @@
   ((keymap-scheme :initform (keymap:make-scheme
                              scheme:emacs *my-keymap*))))
 
-(define-configuration buffer
+(define-configuration (buffer web-buffer)
   ((default-modes (append '(emacs-mode my-mode) %slot-default))
-   (current-zoom-ratio 2)
-   (zoom-ratio-default 2)
+   (current-zoom-ratio 1.75)
+   (zoom-ratio-default 1.75)
    ))
 
-(define-configuration minibuffer
-  ((max-lines 6)
-   (minibuffer-style (cl-css:css
-                      '((* :font-family "monospace,monospace" :font-size "24px" :line-height "28px")
-                        (nyxt::body :border-top "4px solid dimgray" :margin "0" :padding "0 6px")
-                        ("#container" :display "flex" :flex-flow "column" :height "100%")
-                        ("#input" :padding "6px 0" :border-bottom "solid 1px lightgray")
-                        ("#suggestions" :flex-grow "1" :overflow-y "auto" :overflow-x "auto")
-                        ("#cursor" :background-color "gray" :color "white")
-                        ("#prompt" :padding-right "4px" :color "dimgray")
-                        (nyxt::ul :list-style "none" :padding "0" :margin "0")
-                        (nyxt::li :padding "2px")
-                        (nyxt::.marked :background-color "darkgray" :font-weight "bold" :color
-                                       "white")
-                        (nyxt::.selected :background-color "gray" :color "white"))))))
+;; (fullscreen-current-window)
 
-(define-configuration window
-  ((message-buffer-height 24)
-   (message-buffer-style (cl-css:css
-                          '((body
-                             :font-size "24x"
-                             :padding 0
-                             :padding-left "4px"
-                             :margin 0))))))
+(define-configuration minibuffer
+  ((max-lines 8)
+   (minibuffer-style #.(cl-css:css
+                        '((* :font-family "monospace,monospace"
+                             :font-size "18px"
+                             :line-height "22px")
+                          (body :border-top "4px solid dimgray"
+                                :margin "0"
+                                :padding "0 6px")
+                          ("#container" :display "flex"
+                                        :flex-flow "column"
+                                        :height "100%")
+                          ("#input" :padding "6px 0"
+                                    :border-bottom "solid 1px lightgray")
+                          ("#suggestions" :flex-grow "1"
+                                          :overflow-y "auto"
+                                          :overflow-x "auto")
+                          ("#cursor" :background-color "gray"
+                                     :color "white")
+                          ("#prompt" :padding-right "4px"
+                                     :color "dimgray")
+                          (ul :list-style "none"
+                              :padding "0"
+                              :margin "0")
+                          (li :padding "2px")
+                          (.marked :background-color "darkgray"
+                                   :font-weight "bold"
+                                   :color "white")
+                          (.selected :background-color "gray"
+                                     :color "white")))
+                     :documentation "The CSS applied to a minibuffer when it is set-up.")))
+
+;; (define-configuration window
+;;   ((message-buffer-height 24)
+;;    (message-buffer-style (cl-css:css
+;;                           '((body
+;;                              :font-size "24x"
+;;                              :padding 0
+;;                              :padding-left "4px"
+;;                              :margin 0))))))
