69e68efa8c2c86a5d7556f1b6ad06eaaf3a458e1
André Alexandre Gomes <andremegafone@gmail.com>
2020-10-31 22:07:49 +0300

    Added guix channels

---
+++ guix/.config/guix/channels.scm
@@ -0,0 +1,7 @@
+;; (info "(guix) Channels")
+;; (info "(guix) Creating a Channel")
+
+;; (cons (channel
+;;        (name 'variant-packages)
+;;        (url "https://example.org/variant-packages.git"))
+;;       %default-channels)
